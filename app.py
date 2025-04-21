23ca3177f8d70c381e3e98d30a37bf1583bb4e9f
stacatoo <daniel@gsk.co.id>
2025-04-21 22:00:05 +0700

    update api key

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from roboflow import Roboflow
 import tempfile
 
 # Initialize Roboflow
-rf = Roboflow(api_key="YOUR_API_KEY")
+rf = Roboflow(api_key="9T8zDm97SGYJGfcidwAX")
 project = rf.workspace().project("wood-defect-detector")
 model = project.version(1).model
 
